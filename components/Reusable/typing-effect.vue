07f33a1c9b6e7c31feb4ed294fc3538cd408f3af
sokkanha <121841538+sokkanha@users.noreply.github.com>
2025-03-04 13:21:03 +0700

    Add typing effect to quote on index page

---
+++ components/Reusable/typing-effect.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+const props = defineProps<{word: string}>()
+const words = [props.word];
+const displayText = ref("");
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const updateText = () => {
+  const currentWord = words[wordIndex];
+  displayText.value = currentWord.substring(0, charIndex);
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    charIndex++;
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+  } else {
+    // isDeleting = !isDeleting;
+    // if (!isDeleting) wordIndex = (wordIndex + 1) % words.length;
+  }
+
+  setTimeout(updateText, 100);
+};
+
+onMounted(updateText);
+</script>
+
+<template>
+  <span class="text-white">
+    {{ displayText }}<span class="animate-blink">|</span>
+  </span>
+</template>
+
+<style>
+@keyframes blink {
+  50% {
+    opacity: 0;
+  }
+}
+.animate-blink {
+  animation: blink 1s infinite;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -38,8 +38,12 @@
                     I'm a passionate developer with experience in Angular, Tailwind CSS, and creating dynamic web applications.I'm always looking for new challenges to grow my skills and work on impactful projects!
                 </p>
             </div>
+
+            <!-- Quote -->
             <div class="quote flex flex-col gap-2.5  text-gray-300">
-                <p class="border px-1 py-3 text-center">" In the world of technology, we are only limited by our imagination. "</p>
+                <p class="border px-1 py-3 text-center">
+                    <ReusableTypingEffect :word="`  In the world of technology, we are only limited by our imagination. `"/>
+                </p>
                 <span class="border p-2 w-fit self-end">__Unknown</span>
             </div>
         </div>
